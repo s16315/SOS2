b10454a74ce929ec2f3ea4ac702f15578e207850
s16315 <s16315@pjwstk.edu.pl>
2020-01-19 17:15:19 +0100

    aktualizacja

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import {AddRoomComponent} from './components/add-room/add-room.component';
 import {EditRoomComponent} from './components/edit-room/edit-room.component';
 import {RoomsListComponent} from './components/rooms-list/rooms-list.component';
 import {CoursesListComponent} from './components/courses-list/courses-list.component';
-import {AddCourseComponent} from "./components/add-course/add-course.component";
-import {AddLessonComponent} from "./components/add-lesson/add-lesson.component";
-
+import {AddCourseComponent} from './components/add-course/add-course.component';
+import {AddLessonComponent} from './components/add-lesson/add-lesson.component';
+import {StudentsListComponent} from './components/students-list/students-list.component';
+import {AddStudentComponent} from './components/add-student/add-student.component';
+import {ScheduleComponent} from './components/schedule/schedule.component';
 
 
 const routes: Routes = [
@@ -15,7 +17,10 @@ const routes: Routes = [
   {path: 'rooms-list', component: RoomsListComponent},
   {path: 'add-lesson', component: AddLessonComponent},
   {path: 'courses-list', component: CoursesListComponent},
-  {path: 'add-course', component: AddCourseComponent}
+  {path: 'add-course', component: AddCourseComponent},
+  {path: 'student-list', component: StudentsListComponent},
+  {path: 'add-student', component: AddStudentComponent},
+  {path: 'schedule', component: ScheduleComponent}
 ];
 
 @NgModule({
