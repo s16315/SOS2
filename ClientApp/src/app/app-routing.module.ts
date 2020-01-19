e202cc7923a035824952e89b3933a1437987f258
s16315 <s16315@pjwstk.edu.pl>
2020-01-19 12:04:19 +0100

    views

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import {AddRoomComponent} from './components/add-room/add-room.component';
 import {EditRoomComponent} from './components/edit-room/edit-room.component';
 import {RoomsListComponent} from './components/rooms-list/rooms-list.component';
+import {CoursesListComponent} from './components/courses-list/courses-list.component';
+import {AddCourseComponent} from "./components/add-course/add-course.component";
+import {AddLessonComponent} from "./components/add-lesson/add-lesson.component";
 
 
 
 const routes: Routes = [
   {path: 'add-room', component: AddRoomComponent},
   {path: 'edit-room', component: EditRoomComponent},
-  {path: 'rooms-list', component: RoomsListComponent}
+  {path: 'rooms-list', component: RoomsListComponent},
+  {path: 'add-lesson', component: AddLessonComponent},
+  {path: 'courses-list', component: CoursesListComponent},
+  {path: 'add-course', component: AddCourseComponent}
 ];
 
 @NgModule({
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -12,13 +12,31 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {ApiService} from './shared/api.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatCardModule} from "@angular/material";
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { EditCourseComponent } from './components/edit-course/edit-course.component';
+import { AddCourseComponent } from './components/add-course/add-course.component';
+import { AddLessonComponent } from './components/add-lesson/add-lesson.component';
+import { EditLessonComponent } from './components/edit-lesson/edit-lesson.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { StudentsListComponent } from './components/students-list/students-list.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddRoomComponent,
     EditRoomComponent,
-    RoomsListComponent
+    RoomsListComponent,
+    CoursesListComponent,
+    EditCourseComponent,
+    AddCourseComponent,
+    AddLessonComponent,
+    EditLessonComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentsListComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
